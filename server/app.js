49a50e2f557cd29e53428131a25b8ffd028303b1
Pavlo Ovchynnykov <gusovchpa@student.gu.se>
2024-09-13 13:14:01 +0200

    #3: create basic req/res funcitonality for Users entity

--- server/app.js
+++ server/app.js
@@ -29,10 +29,68 @@ app.use(morgan('dev'));
 app.options('*', cors());
 app.use(cors());
 
+
+let users = [];
+
 // Import routes
 app.get('/api', function(req, res) {
     res.json({'message': 'Welcome to your DIT342 backend ExpressJS project!'});
 });
+
+app.post('/users', function(req, res) {
+    let newUser = {
+        "id": users.length,
+        "username": req.body.username,
+        "password": req.body.password
+    }
+    users.push(newUser);
+    res.status(201).json(newUser);
+});
+
+app.get('/users', function(req, res) {
+    res.json({"users": users});
+})
+
+app.get('/users/:id', function(req, res) {
+    res.json(users[req.params.id])
+})
+
+app.put('/users/:id', function(req, res) {
+    let id = req.params.id;
+    let updated_user = {
+        "id": id,
+        "username": req.body.username,
+        "password": req.body.password
+    }
+    users[id] = updated_user;
+    res.json(updated_user);
+})
+
+app.patch('/users/:id', function(req, res) {
+    let id = req.params.id;
+    let user = users[id];
+    let updated_user = {
+        "id": id,
+        "username": req.body.username || user.username,
+        "password": req.body.password || user.password
+    }
+    users[id] = updated_user;
+    res.json(updated_user);
+})
+
+app.delete('/users/:id', function(req, res) {
+    let id = req.params.id;
+    let user = users[id];
+    delete user[id];
+    res.json(user);
+})
+
+app.delete('/users', function(req, res) {
+    users = [];
+    res.json("Users deleted");
+})
+
+
 
 // Catch all non-error handler for api (i.e., 404 Not Found)
 app.use('/api/*', function (req, res) {
