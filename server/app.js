651b05abe722c2ba412051fdf1b891abf2219164
Stefan Tram <stefantr@student.chalmers.se>
2024-09-17 13:14:35 +0200

    #3: create basic req/res funcitonality for Admins entity

--- server/app.js
+++ server/app.js
@@ -153,6 +153,63 @@ app.delete('/post', function(req, res) {
 
 //#endregion
 
+//#region Admin
+
+app.post('/admins', function(req, res) {
+    let newAdmin = {
+        "id": admins.length,
+        "username": req.body.username,
+        "password": req.body.password
+    }
+    admins.push(newAdmin);
+    res.status(201).json(newAdmin);
+});
+
+app.get('/admins', function(req, res) {
+    res.json({"admins": admins});
+})
+
+app.get('/admins/:id', function(req, res) {
+    res.json(admins[req.params.id])
+})
+
+app.put('/admins/:id', function(req, res) {
+    let id = req.params.id;
+    let updated_admins = {
+        "id": id,
+        "username": req.body.username,
+        "password": req.body.password
+    }
+    admins[id] = updated_admin;
+    res.json(updated_admin);
+})
+
+app.patch('/admins/:id', function(req, res) {
+    let id = req.params.id;
+    let admin = admins[id];
+    let updated_admin = {
+        "id": id,
+        "username": req.body.username || admin.username,
+        "password": req.body.password || admin.password
+    }
+    admins[id] = updated_admin;
+    res.json(updated_admin);
+})
+
+app.delete('/admins/:id', function(req, res) {
+    let id = req.params.id;
+    let admin = admins[id];
+    delete admin[id];
+    res.json(admin);
+})
+
+app.delete('/admins', function(req, res) {
+    admins = [];
+    res.json("Admins deleted");
+})
+
+//#endregion
+
 // Catch all non-error handler for api (i.e., 404 Not Found)
 app.use('/api/*', function (req, res) {
     res.status(404).json({ 'message': 'Not Found' });
