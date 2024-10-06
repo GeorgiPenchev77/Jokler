36e1f57653d65cea248ac02a773b084e1fa87ca9
Stefan Tram <stefantr@student.chalmers.se>
2024-10-07 00:57:55 +0200

    #20 Base template

---
+++ client/src/views/Profile.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <h1>Your Profile</h1>
+      <p>Welcome to your profile page!</p>
+      <!-- Profile content -->
+    </div>
+</template>
+  
+  <script>
+  export default {
+    name: 'Profile'
+  }
+  </script>
