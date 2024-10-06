fb6a640ea19471b6d895659b33543e199c3583ed
Stefan Tram <stefantr@student.chalmers.se>
2024-10-07 00:56:50 +0200

    #20 Setup route

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from './views/Home.vue'
+import Profile from './views/Profile.vue'
 
 const routes = [
   { path: '/', name: 'home', component: Home },
